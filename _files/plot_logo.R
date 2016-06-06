6978f26bcc2cd6b0c334d17db0ffceffc2c7aa72
HeidiSeibold <heidi.seibold@uzh.ch>
2016-06-06 18:21:07 +0200

    create logo in separate R file

---
+++ _files/plot_logo.R
@@ -0,0 +1,12 @@
+
+library("ggplot2")
+letter <- "R"
+df <- data.frame(x = 1, y = 1, label = "R")
+
+p <- ggplot(df, aes(x = x, y = y, label = label)) +
+  geom_text(size = 100, color = "white") +
+  theme(panel.background = element_rect(fill = '#E11A27'),
+        panel.grid.minor = element_line(colour = '#E11A27'),
+        panel.grid.major = element_line(colour = '#E11A27'))
+
+ggsave("_files/plot_logo.pdf", plot = p, width = 7, height = 7)
